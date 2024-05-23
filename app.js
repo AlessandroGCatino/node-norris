cc318c60dc7886743c44345cf51e4a9a739dcd3b
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-05-23 16:35:37 +0200

    bonus: implementata funzione ricorsiva e non asincrona

--- app.js
+++ app.js
@@ -14,29 +14,46 @@ function changeDatas(fileName, newData) {
 }
 
 // funzione asincrona perchè abbiamo bisogno di attendere la risposta dal fetch per poter comparare correttamente il nuovo joke con quelli presenti nel DB
-async function getJoke(){
-    let flag = true;
+// async function getJoke(){
+//     let flag = true;
 
-    while (flag) {
-        try {
-            // aspetto risposta dal fetch per poter continuare il ciclo correttamente
-            let res = await fetch("https://api.chucknorris.io/jokes/random");
-            let data = await res.json();
-            let joke = data.value;
+//     while (flag) {
+//         try {
+//             // aspetto risposta dal fetch per poter continuare il ciclo correttamente
+//             let res = await fetch("https://api.chucknorris.io/jokes/random");
+//             let data = await res.json();
+//             let joke = data.value;
 
-            if (!chuckDb.includes(joke)) {
-                changeDatas("norrisDb", [...chuckDb, joke]);
-                console.log("Nuova battuta: ", joke);
-                flag = false;
-            } else {
-                console.log("Già inserito")
-            }
-        } catch (error) {
-            console.error('Errore:', error);
+//             if (!chuckDb.includes(joke)) {
+//                 changeDatas("norrisDb", [...chuckDb, joke]);
+//                 console.log("Nuova battuta: ", joke);
+//                 flag = false;
+//             } else {
+//                 console.log("Già inserito")
+//             }
+//         } catch (error) {
+//             console.error('Errore:', error);
+//         }
+//     }
+// }
+
+
+// implementata con funzione ricorsiva e non asincrona
+function getJoke(){
+    fetch("https://api.chucknorris.io/jokes/random")
+    .then(res => res.json())
+    .then(data => {
+        let joke = data.value;
+        if (!chuckDb.includes(joke)) {
+            changeDatas("norrisDb", [...chuckDb, joke]);
+            console.log("Nuova battuta: ", joke);
+            flag = false;
+        } else {
+            console.log("Ops, vecchia battuta, ne trovo un'altra")
+            getJoke()
         }
-    }
+    })
 }
-
+getJoke();
 const chuckDb = readDatas("norrisDb")
 console.log("Lista battute: ", chuckDb)
-getJoke()
